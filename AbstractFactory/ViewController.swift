8b1cd310e18f9a70e951c1fafb4f3f81975e57b9
AyanMagerramli <136072693+AyanMagerramli@users.noreply.github.com>
2024-09-15 17:01:18 +0400

    initial commit

---
+++ AbstractFactory/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AbstractFactory
+//
+//  Created by Ayan on 15.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func orderKichenTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var orderBedroomTapped: UIImageView!
+}
+
